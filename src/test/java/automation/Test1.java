fc76df61a0cda827d3c2431509ca5e82676b4c8e
bharathram11 <bharathpandya1122@gmail.com>
2024-11-29 13:38:23 +0530

    Initial commit

--- src/test/java/automation/Test1.java
+++ src/test/java/automation/Test1.java
@@ -1,47 +1,17 @@
 package automation;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
-import pages.Registration;
-
+//This is the specific class being imported.you're making the ChromeDriver class accessible in your Java code
 public class Test1 {
-	
-	WebDriver driver;
-	@BeforeMethod
-	void setup()
-	{
-		driver=new ChromeDriver();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.manage().window().maximize();
+
+	public static void main(String[] args) throws Exception {
+		/*Instance class :create an object and then call properties and
+		   methods of the class using the object */
+		//way-1 to open a chrome browser
+		ChromeDriver driver=new ChromeDriver();
+		driver.close();
+	   
+	   
 	}
-	@Test
-	void test()
-	{
-		Registration rg=new Registration(driver);
-		rg.username("Admin");
-		rg.pass("admin123");
-		rg.click();
-		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
-	}
-	@AfterMethod
-	void teardown()
-	{
-		driver.quit();
-	}
-	
 }
---
+++ src/test/java/automation/Test12.java
@@ -0,0 +1,19 @@
+package automation;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Test12 {
+
+	public static void main(String[] args) throws Exception {
+		
+		//3 way of code to  open a firefox driver
+		FirefoxDriver driver1=new FirefoxDriver();
+		WebDriver driver2 =new FirefoxDriver();
+		RemoteWebDriver driver3=new FirefoxDriver();
+	
+		
+	}
+}
